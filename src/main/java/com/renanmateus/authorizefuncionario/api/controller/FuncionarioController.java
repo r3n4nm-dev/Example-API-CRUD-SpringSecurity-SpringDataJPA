d8256477d4e8dac63cb95b68b1b25b430300eb8d
r3n4nm-dev <85502192+r3n4nm-dev@users.noreply.github.com>
2021-06-08 09:18:43 -0300

    Update FuncionarioController.java

--- src/main/java/com/renanmateus/authorizefuncionario/api/controller/FuncionarioController.java
+++ src/main/java/com/renanmateus/authorizefuncionario/api/controller/FuncionarioController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -57,7 +57,7 @@ public class FuncionarioController {
 		
 	}
 	
-	@PatchMapping("/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<?> atualizar(@PathVariable long id, @RequestBody Funcionario funcionario) {
 		Optional<Funcionario> funcionarioAtual = funcionarioRepository.findById(id);
 		if (funcionarioAtual.isEmpty()) {
